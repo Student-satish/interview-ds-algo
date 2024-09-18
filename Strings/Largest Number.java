a86674fb61b5fbfb7bae4cd08b382abe5945263b
Student-satish <ksatish23819@gmail.com>
2024-09-18 18:21:32 +0530

    largest num

--- Strings/Largest Number.java
+++ Strings/Largest Number.java
@@ -6,6 +6,15 @@ class Solution {
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.toString(nums[i]);
         }
+        // int result = (b + a).compareTo(a + b);
+        // result is positve a comes after b
+        // result is negative a comes before b
+        // result is positve both strings are same
+        // ["3","30"]
+        // a = 3
+        // b = 30
+        // b+a = 303 , a + b = 330 , (b+a < a+b) result becomes negative
+        // result is negative that means [b,a] forms less number than [a,b] so a comes before b which is [a,b];
         // here we custom sorting based on concatenation of two strings
         Arrays.sort(arr, (a, b) -> (b + a).compareTo(a + b));
         // when an array contains all zeroes
