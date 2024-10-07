043a2bd0edd261fc0daec503e9d376432daca5d9
Student-satish <ksatish23819@gmail.com>
2024-10-07 11:07:37 +0530

    minLen after removing substrings

--- Stack/Minimum String Length After Removing Substrings.java
+++ Stack/Minimum String Length After Removing Substrings.java
@@ -15,3 +15,23 @@ class Solution {
         return minLen;
     }
 }
+// in place modifcation
+class Solution {
+    public int minLength(String s) {
+        int n = s.length();
+        char[] temp = s.toCharArray();
+        int i = 0; // write
+        int j = 1; // read
+        // whatever j read just write that on i
+        for(j = 1; j < n; j++) {
+            if((i >= 0) && ((temp[i] == 'A' && temp[j] == 'B') || (temp[i] == 'C' && temp[j] == 'D'))) {
+                i--;
+            }else{
+                i++;
+                temp[i] = temp[j];
+            }
+        }
+        
+        return i + 1;
+    }
+}
