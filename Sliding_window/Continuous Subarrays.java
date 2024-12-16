717b08d5bcd428c48b50bd0e9fc91414b36ba6dc
Student-satish <ksatish23819@gmail.com>
2024-12-16 19:53:08 +0530

    dq

--- Sliding_window/Continuous Subarrays.java
+++ Sliding_window/Continuous Subarrays.java
@@ -49,6 +49,41 @@ class Solution {
     }
 }
 
+// deque approach 
+class Solution {
+    public long continuousSubarrays(int[] nums) {
+        Deque<Integer> maxDq = new ArrayDeque<>();
+        Deque<Integer> minDq = new ArrayDeque<>();
+        int left = 0 , right = 0 , n = nums.length;
+        long ans = 0;
+        while(right < n) {
+            while(!minDq.isEmpty() && nums[right] <= nums[minDq.peekLast()]) {
+                minDq.pollLast();
+            }
+            while(!maxDq.isEmpty() && nums[right] >= nums[maxDq.peekLast()]) {
+                maxDq.pollLast();
+            }
+            minDq.offerLast(right);
+            maxDq.offerLast(right);
+            while(!minDq.isEmpty() && !maxDq.isEmpty() && nums[maxDq.peekFirst()] - nums[minDq.peekFirst()] > 2) {
+                int jump = Math.min(maxDq.peekFirst(),minDq.peekFirst());
+                left = jump + 1;
+                while(!minDq.isEmpty() && minDq.peekFirst() < left) {
+                    minDq.pollFirst();
+                }
+                while(!maxDq.isEmpty() && maxDq.peekFirst() < left) {
+                    maxDq.pollFirst();
+                }
+            }
+            ans += right - left + 1;
+            right++;
+        }
+        return ans;
+    }
+}
+
+
+
 // O(n) solution two pointer
 
 class Solution {
