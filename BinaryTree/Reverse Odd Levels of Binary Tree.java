8ee0aa079cde6eae2023e8142a84a6460e9c75d8
Student-satish <ksatish23819@gmail.com>
2024-12-20 10:10:09 +0530

    PBT

--- BinaryTree/Reverse Odd Levels of Binary Tree.java
+++ BinaryTree/Reverse Odd Levels of Binary Tree.java
@@ -1,4 +1,6 @@
 import java.util.*;
+
+// BFS
 class Solution {
     public TreeNode reverseOddLevels(TreeNode root) {
         Queue<TreeNode> q = new LinkedList<>();
@@ -36,6 +38,29 @@ class Solution {
             level++;
         }
 
+        return root;
+    }
+}
+
+// DFS
+
+class Solution {
+    public void traverseDFS(TreeNode leftChild,TreeNode rightChild,int level) {
+        if(leftChild == null || rightChild == null) {
+            return;
+        }
+
+        if(level % 2 == 0) {
+            int temp = leftChild.val;
+            leftChild.val = rightChild.val;
+            rightChild.val = temp;
+        }
+        
+        traverseDFS(leftChild.left,rightChild.right,level+1);
+        traverseDFS(leftChild.right,rightChild.left,level+1);
+    }
+    public TreeNode reverseOddLevels(TreeNode root) {
+        traverseDFS(root.left,root.right,0);
         return root;
     }
 }
